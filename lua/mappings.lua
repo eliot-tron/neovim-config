1bdb73fc179594a686e13b0fa16cabedf112fcc8
eliot-tron <eliottron@gmail.com>
2024-06-07 11:31:10 +0200

    mapping for nerdtree

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -149,3 +149,8 @@ keyset("n", "<space>j", ":<C-u>CocNext<cr>", opts)
 keyset("n", "<space>k", ":<C-u>CocPrev<cr>", opts)
 -- Resume latest coc list
 keyset("n", "<space>p", ":<C-u>CocListResume<cr>", opts)
+
+
+-- NERTTreeTab --
+
+keyset("n", "<leader>nt", ":NERDTreeToggle<cr>", {silent = true} )
